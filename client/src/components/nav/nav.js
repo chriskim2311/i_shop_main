e552c7b6882ffb86a3f2b20a930138714f48d720
chriskim2311 <chriskim2311@gmail.com>
2018-12-28 12:31:31 -0800

    Completed initial navbar and side nav functionality

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {Link} from 'react-router-dom'
+import './nav.scss';
+import SideNav from './side_nav'
 
 class Nav extends Component{
 
@@ -35,9 +38,63 @@ class Nav extends Component{
         ]
     };
 
+
+    renderSignOut(){
+       return <button className="btn wave-effect waves-light blue" onClick={() => console.log("signout clicked")}>Sign Out</button>
+    }
+
+    buildLink(link){
+        return <li key={link.to}>
+         <Link to={link.to}>{link.text}</Link>
+         </li>
+
+
+    }
+    renderLinks = () => {
+        const auth = true;
+        let authLinks = [];
+        const {auth: navAuth, common, nonAuth } = this.state
+        const commonLinks = common.map(this.buildLink);
+
+        if(auth) {
+            authLinks = navAuth.map(this.buildLink);
+            authLinks.push(<li key="/sign-out" className="sign-out">{this.renderSignOut()}</li>)
+        } else {
+            authLinks = nonAuth.map(this.buildLink);
+        }
+
+        return [...commonLinks, ...authLinks];
+
+
+        
+    }
+    getSideNavRef(e) {
+        console.log("side nav ref:", e)
+    }
+
     render(){
         return (
-            <nav className="blue-grey darken-2"></nav>
+            <Fragment>
+                            <nav className="blue-grey darken-2">
+            <div className="nav-wrapper">
+            <Link className="brand-logo" to="/">iShop</Link>
+            <Link to="#" data-target="side-nav" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+            </Link>
+
+
+
+            <ul className= "right hide-on-med-and-down">
+                {this.renderLinks()}
+            </ul>
+            </div>
+            </nav>
+
+
+            <SideNav renderLinks={this.renderLinks}/>
+
+            </Fragment>
+
         );
     }
 }
